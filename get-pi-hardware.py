d64af5c95ce0d07fcb869085760d0c9de897b419
Ruslan Khozinov <rkhozinov@mirantis.com>
2015-07-22 20:59:24 +0300

    Added some cosmetic changes

--- get-pi-hardware.py
+++ get-pi-hardware.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python
 
-#import sys, getopt
 from bs4 import BeautifulSoup
 import os
 import requests
 
-USER=os.getenv('USER')
-PASSWORD=os.getenv('PASSWORD')
+user=os.getenv('USER')
+password=os.getenv('PASSWORD')
+url = "https://mirantis.jira.com/wiki/display/PRT/Hardware+for+PI+team"
 
-if not USER :
-    print "can't find USER system variable"
+if not user :
+    print "can't find user system variable"
     exit(1)
 
-if not PASSWORD:
-    print "can't find PASSWORD system variable"
+if not password:
+    print "can't find password system variable"
     exit(1)
 
 def main():
-      r = requests.get("https://mirantis.jira.com/wiki/display/PRT/Hardware+for+PI+team", auth=(USER, PASSWORD))
-      if r.status_code != 200:
-          print "can't get content"
-          exit(1) 
-      html_data = r.text
+    r = requests.get(url, auth=(user, password))
+    if r.status_code != 200:
+        print "can't get content"
+        exit(1) 
+    
+    html_data = r.text
 
-      table_data = [[cell.text for cell in row("td")]
-                               for row in BeautifulSoup(html_data,"html5lib")("tr")]
-      
-      for item in table_data:
-          if item:
-            print u"{} {}".format(item[0], item[4]).encode('ascii','ignore')
+    table_data = [[cell.text for cell in row("td")]
+                             for row in BeautifulSoup(html_data, "html5lib")("tr")]
+    
+    for item in table_data:
+        if item:
+          print u"{} {}".format(item[0], item[4]).encode('ascii', 'ignore')
 
 if __name__ == "__main__":
    main()
